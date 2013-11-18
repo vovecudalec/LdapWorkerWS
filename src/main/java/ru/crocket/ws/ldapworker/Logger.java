33cfe79ff20ccffa7238f8641e4da18ed30d455a
zuev <vovecudalec@gmail.com>
2013-11-18 13:45:26 +0400

    notwork

---
+++ src/main/java/ru/crocket/ws/ldapworker/Logger.java
@@ -0,0 +1,12 @@
+package ru.crocket.ws.ldapworker;
+
+
+public class Logger {
+    public void log_before(){
+        System.out.println("Лог до метода");
+    }
+
+    public void log_after(){
+        System.out.println("Лог после метода");
+    }
+}
